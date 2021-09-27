d6263761ef9b7b11bd00d9bfa8119802d5257ad3
ZOV <442683629@ZOV.com>
2021-09-27 14:19:43 +0800

    hot-fix-commit2

--- src/main/java/com/Spring01/test/Start04.java
+++ src/main/java/com/Spring01/test/Start04.java
@@ -7,6 +7,7 @@ public class Start04 {
         System.out.println("hello 杰哥");
 
         System.out.println("第四个版本的测试");
-        System.out.println("hot-fix的第二次修改");
+        System.out.println("hot-fix");
+
     }
 }
