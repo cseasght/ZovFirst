4ced5050e1f1491e239db636f2bb713293670d70
ZOV <442683629@ZOV.com>
2021-09-27 11:04:56 +0800

    second_commit

--- src/main/java/com/Spring01/test/Start04.java
+++ src/main/java/com/Spring01/test/Start04.java
@@ -3,5 +3,6 @@ package com.Spring01.test;
 public class Start04 {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello 阿伟");
     }
 }
